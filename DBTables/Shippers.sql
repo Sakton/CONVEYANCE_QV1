28efed817a5d8cd5db6c041a92282bc29468d16c
sakton <sakton@mail.ru>
2021-07-19 12:52:24 +0300

    11

--- DBTables/Shippers.sql
+++ DBTables/Shippers.sql
@@ -1,30 +1,37 @@
 -- отправитель грузов (shipper)
 
+-- СОЗДАТЬ ОБЛАТЬ ИМЕН "ГРУЗОТПРАВИТЕЛЬ"
 CREATE SCHEMA IF NOT EXISTS shipper;
 
 
--- ***********
+-- ТАБЛИЦА "ГРУЗОТПРАВИТЕЛИ"
 CREATE TABLE shipper.shippers (
-	shipper_id SERIAL UNIQUE, -- PK
-	shipper_name VARCHAR(200)
+shipper_id SERIAL UNIQUE, -- PK
+shipper_name VARCHAR(200) -- наименование заказчика ( грузоотправитель )
 );
 
 
+-- ПРОЦЕДУРА ВСТАВКИ В БАЗУ ГРУЗОТПРАВИТЕЛЯ, ЕСЛИ ТАКОЙ УЖЕ ЕСТЬ ТО ИГНОРИРОВАТЬ ВСТАВКУ
+CREATE PROCEDURE shipper.addShipper() LANGUAGE SQL AS
+$$
+
+$$;
+
 --тестировать
--- ***********
+-- ФУНКЦИЯ ПОЛУЧЕНИЯ ID ОТПРАВИТЕЛЯ ПО ИМЕНИ, ЕСЛИ ТАКОГО ОПРАВИТЕЛЯ НЕТ ТО ЕГО ВСТАВКА В БАЗУ
 CREATE OR REPLACE FUNCTION shipper.getShipper_id( shipperName VARCHAR ) RETURNS INTEGER AS
 $$
 DECLARE id INTEGER;
-	BEGIN
-		id = ( SELECT shipper_id FROM shipper.shippers WHERE  shipper_name = shipperName );
-		IF ( id IS NULL ) THEN
-			BEGIN
-				INSERT INTO shipper.shippers ( shipper_name ) VALUES (shipperName);
-				id = ( SELECT lastval( ) ) ;
-			END;
-		END IF;
-		RETURN id;
-	END;
+BEGIN
+id = ( SELECT shipper_id FROM shipper.shippers WHERE  shipper_name = shipperName );
+IF ( id IS NULL ) THEN
+BEGIN
+INSERT INTO shipper.shippers ( shipper_name ) VALUES ( shipperName );
+id = ( SELECT lastval( ) ) ;
+END;
+END IF;
+RETURN id;
+END;
 $$
 LANGUAGE plpgsql;
 
