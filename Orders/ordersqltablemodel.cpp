e22250aa8aa3ad0e02911ab1396ecb16849b6a63
sakton <sakton@mail.ru>
2021-09-02 15:25:53 +0300

    dump_order

--- Orders/ordersqltablemodel.cpp
+++ Orders/ordersqltablemodel.cpp
@@ -3,6 +3,8 @@
 OrderSqlTableModel::OrderSqlTableModel( QObject * parent, const QSqlDatabase & db ) : QSqlTableModel( parent, db ) {}
 
 QVariant OrderSqlTableModel::data( const QModelIndex & index, int role ) const {
+  if ( index.column( ) == 9 && role == Qt::DisplayRole ) {
+  }
   if ( role == Qt::TextAlignmentRole ) {
     return Qt::AlignCenter;
   }
