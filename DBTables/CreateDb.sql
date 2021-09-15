505c73ac15e5cbb17441ca693cb43e0b706a9cd6
sakton <sakton@mail.ru>
2021-09-15 16:22:52 +0300

    img_fur

--- DBTables/CreateDb.sql
+++ DBTables/CreateDb.sql
@@ -1,9 +1,10 @@
 -- ЗАПРОС В СУБД НА НАЛИЧИЕ БАЗЫ ДАННЫХ
 -- SELECT 1 AS res FROM pg_database WHERE datname = 'demo_coveyance_db';
-SELECT 1 AS res FROM pg_database WHERE datname = '%1';
+-- SELECT 1 AS res FROM pg_database WHERE datname = '%1';
 
 -- создание базы
 
 CREATE DATABASE demo_coveyance_db 
 	WITH TEMPLATE = template0 ENCODING = 'UTF8'
              LOCALE = 'Russian_Russia.1251';
+
